02e52d08785a0b7a68fb08a28439255216b49d37
RonexLemon <ronexondimu@gmail.com>
2025-02-14 13:35:56 +0300

    config gunicorn

--- Vola_Agent/fetch_data.py
+++ Vola_Agent/fetch_data.py
@@ -9,7 +9,7 @@ HEADERS = {
 }
 
 
-def fetch_price_history(token_id ="aave",currency="usd", days=1):
+def fetch_price_history(token_id ="near",currency="usd", days=1):
     url = f"{COINGECKO_API}/coins/{token_id}/market_chart?vs_currency={currency}&days={days}"
     params = {"vs_currency": currency, "days": days}
     response = requests.get(url, headers=HEADERS, params=params)
